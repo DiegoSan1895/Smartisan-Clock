902144a08f38b1079cfa142f9cf2dc027694c15b
SanDiego <xyq_since1895@163.com>
2016-03-09 15:58:28 +0800

    ready

--- Smartisan-Clock/AppDelegate.swift
+++ Smartisan-Clock/AppDelegate.swift
@@ -17,26 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        testXMLParser()
         return true
     }
     
     func setAppearance(){
-        
     }
     
-    func testXMLParser(){
-        let URL = NSBundle.mainBundle().URLForResource("city_all", withExtension: "xml")!
-        let parser = NSXMLParser(contentsOfURL:URL)
-        parser?.delegate = self
-        parser?.parse()
-    }
 }
 
-extension AppDelegate: NSXMLParserDelegate{
-    func parserDidStartDocument(parser: NSXMLParser) {
-        print("start parse")
-    }
-    
-    
-}
---
+++ Smartisan-Clock/ClockView.swift
@@ -0,0 +1,21 @@
+//
+//  ClockView.swift
+//  Smartisan-Clock
+//
+//  Created by DiegoSan on 3/9/16.
+//  Copyright © 2016 DiegoSan. All rights reserved.
+//
+
+import UIKit
+
+class ClockView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
